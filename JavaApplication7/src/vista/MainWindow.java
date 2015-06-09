d67a553936eafb255d6f9af8cd259b57302c2b89
alexis <alexis@byte77>
2015-06-09 12:00:54 -0300

    Solución modelo tabla de pestaña 1.

--- JavaApplication7/src/vista/MainWindow.java
+++ JavaApplication7/src/vista/MainWindow.java
@@ -143,9 +143,16 @@ public class MainWindow extends javax.swing.JFrame {
             Class[] types = new Class [] {
                 java.lang.Integer.class, java.lang.String.class, java.lang.Integer.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, true, true
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         jScrollPane4.setViewportView(jTable_productos_disponibles);
@@ -405,7 +412,8 @@ public class MainWindow extends javax.swing.JFrame {
         TF_precio.setText("");
         TF_stock.setText("");
         
-        DefaultTableModel dtm = (DefaultTableModel)jTable_productos_disponibles.getModel();
+        String[] columnNames = {"Código","Nombre","Stock"};
+        DefaultTableModel dtm = new DefaultTableModel(columnNames, 0);
         
         List<ProductoBean> tmp = this.productoDAO.listarProductos();
         for (ProductoBean bean : tmp) {
